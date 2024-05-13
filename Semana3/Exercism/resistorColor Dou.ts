cd56ed5274d0ee0295b75b5287352c0859b61e47
Victoria-94 <146044863+Victoria-94@users.noreply.github.com>
2024-05-12 21:11:58 -0500

    5 semana

--- Semana3/Exercism/resistorColor Dou.ts
+++ Semana3/Exercism/resistorColor Dou.ts
@@ -1,4 +1,7 @@
 const colourValues = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 export function decodedValue(bands:string[]):number {
    return colourValues.indexOf(bands[0]) * 10 + colourValues.indexOf(bands[1])
-}
+   //respasar indexOf
+}//como llamar a la funcion 
+
+decodedValue(["black","red","yellow"]);
---
+++ Semana5/Exercism/AgeSpace.ts
@@ -0,0 +1,16 @@
+interface Planets {
+    [key: string]: number
+}
+const RATIOS: Planets = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+}
+export function age(planet: string, seconds: number): number {
+    return Number((seconds / 31557600 / RATIOS[planet]).toFixed(2))
+}
